ca098b6cf48c86d77e7ca27ba266bb7eb25cc73a
hanchorong <hancrong@gmail.com>
2023-04-05 22:49:52 +0900

    끝말잇기_class형

--- lecture/WordRelay_class.jsx
+++ lecture/WordRelay_class.jsx
@@ -7,14 +7,12 @@ class WordRelay extends Component {
     value: "",
     result: "",
   };
+
   onSubmitForm = (e) => {
     e.preventDefault();
+    console.log(this.state.word.length - 1);
     if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        word: this.state.value,
-        result: "정답",
-        value: "",
-      });
+      this.setState({ result: "딩동댕", word: this.state.value, value: "" });
       this.input.focus();
     } else {
       this.setState({
@@ -24,25 +22,27 @@ class WordRelay extends Component {
       this.input.focus();
     }
   };
-  onChange = (e) => {
+  onChangeInput = (e) => {
     this.setState({ value: e.target.value });
   };
+
   input;
   onRefInput = (c) => {
     this.input = c;
   };
+
   render() {
     return (
       <>
         <div>{this.state.word}</div>
         <form onSubmit={this.onSubmitForm}>
           <input
-            type="text"
             ref={this.onRefInput}
-            onChange={this.onChange}
-            value={this.state.value}
-          />
-          <button>입력</button>
+            value={this.value}
+            onChange={this.onChangeInput}
+            type="text"
+          ></input>
+          <button>입력!!</button>
         </form>
         <div>{this.state.result}</div>
       </>
--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+// const LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   name: "wordrelay-setting",
@@ -10,8 +12,38 @@ module.exports = {
   entry: {
     app: ["./client"],
   }, //입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"],
+                }, //서비스 타깃이 되는 브라우저를 적어줄수있다.
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+  //아래 plugins는 익스텐션 같은것
+  plugins: [new ReactRefreshPlugin()],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
+    publicPath: "/dist",
   }, //출력
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
 };
