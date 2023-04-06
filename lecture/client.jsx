a5a3777f99678ac02b57baaf49ce57331c7f366f
hanchorong <hancrong@gmail.com>
2023-04-06 16:37:37 +0900

    숫자야구 class형

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,6 +1,12 @@
-const React = require("react");
-const ReactDom = require("react-dom");
+// const React = require("react");
+// const ReactDom = require("react-dom");
 
-const WordRelay = require("./WordRelay");
+// const NumberBaseball = require("./NumberBaseball");
 
-ReactDom.render(<WordRelay />, document.querySelector("#root"));
+// ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
+import React from "react";
+import ReactDom from "react-dom";
+
+import NumberBaseball from "./NumberBaseball";
+
+ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
